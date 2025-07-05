548989c28b971d7655dfb9b3d39ebf1379d5839b
FATUMA003 <fatmakhamisissa@gmail.com>
2025-07-05 14:05:03 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,59 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
-android {
-    namespace = "com.example.projecttracker"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.projecttracker"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
-dependencies {
-    // AndroidX
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // OkHttp Logger (optional)
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation(libs.firebase.firestore.ktx)
-    implementation(libs.androidx.activity)
-
-    // Test
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
+// No dependencies or implementation here!
